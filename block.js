764ecc538743456444e3e15da00ceb28b22f3bd5
Alexandra de Paula <alexandra.paula@aluno.ufca.edu.br>
2024-11-07 20:08:51 -0300

    Adciona prova de trabalho e nonce

--- block.js
+++ block.js
@@ -1,17 +1,27 @@
 const crypto = require('crypto');
 
 class Block {
-  constructor(timestamp, transactions, previousHash = '') {
+  constructor(timestamp, transactions, previousHash = '', difficulty = 2) {
     this.timestamp = timestamp;
     this.transactions = transactions;
     this.previousHash = previousHash;
+    this.nonce = 0;
+    this.difficulty = difficulty;
     this.hash = this.calculateHash();
   }
 
   calculateHash() {
-    const data = this.previousHash + this.timestamp + JSON.stringify(this.transactions);
+    const data = this.previousHash + this.timestamp + JSON.stringify(this.transactions)+ this.nonce;
     return crypto.createHash('sha256').update(data).digest('hex');
   }
+}
+
+mineBlock() {
+  while (this.hash.substring(0, this.difficulty) !== Array(this.difficulty + 1).join("0")) {
+    this.nonce++;
+    this.hash = this.calculateHash();
+  }
+  console.log(`Bloco minerado: ${this.hash}`);
 }
 
 module.exports = Block;
