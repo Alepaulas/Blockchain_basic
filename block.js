f24f328c0058788d231a22c59e0fd651827b04cb
Alexandra de Paula <alexandra.paula@aluno.ufca.edu.br>
2024-11-07 20:28:24 -0300

    update

--- block.js
+++ block.js
@@ -13,15 +13,17 @@ class Block {
   calculateHash() {
     const data = this.previousHash + this.timestamp + JSON.stringify(this.transactions)+ this.nonce;
     return crypto.createHash('sha256').update(data).digest('hex');
-  }
+
 }
 
+
 mineBlock() {
-  while (this.hash.substring(0, this.difficulty) !== Array(this.difficulty + 1).join("0")) {
-    this.nonce++;
-    this.hash = this.calculateHash();
-  }
-  console.log(`Bloco minerado: ${this.hash}`);
+   while (this.hash.substring(0, this.difficulty) !== Array(this.difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+   }
+   console.log(`Bloco minerado: ${this.hash}`);
+ }
 }
 
 module.exports = Block;
